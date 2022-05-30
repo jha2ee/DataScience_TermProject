a7a729ecfdfc692947b113bfb77a0705fa43d0ba
jha2ee <jha2ee99@gmail.com>
2022-05-30 20:06:28 +0900

    Update main.py
    model  추가 (random forest regressor)

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 from sklearn.model_selection import train_test_split
 
 
-from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestRegressor
 
 import warnings 
 warnings.filterwarnings("ignore")
@@ -25,16 +24,17 @@ Data exploration(For dataset description)
 # 3. Data cleaning - set NaN values and check with isnull()
 df['Diabetic'] = df['Diabetic'].replace('No, borderline diabetes', np.NaN)
 df['Diabetic'] = df['Diabetic'].replace('Yes (during pregnancy)', np.NaN)
+
 # check number of NaN values created
+print("===Check isnull()===")
 print(df.isnull().sum().sort_values(ascending=False))
 
 # 4. Data inspection
-print(df.head())
-print("describe")
+print("===describe===")
 print(df.describe())
-print("info")
+print("===info===")
 print(df.info())
-print("nunique")
+print("===nunique===")
 print(df.nunique())
 
 # 5. Cleaning dirty data (Our dirty data is in column 'Diabetic')
@@ -57,28 +57,21 @@ df['AgeCategory'] = df['AgeCategory'].replace('70-74', 72)
 df['AgeCategory'] = df['AgeCategory'].replace('75-79', 77)
 df['AgeCategory'] = df['AgeCategory'].replace('80 or older', 80)
 
-# 6. Data Visualization
-def histo_visualize(data, feature):
-    #fig, ax = plt.subplots(figsize = (13, 6))
-    ax.hist(df[df["HeartDisease"]==1][feature], bins=10, alpha=0.5, color="#86E57F", label="HeartDisease")
-    ax.hist(df[df["HeartDisease"]==0][feature], bins=10, alpha=0.5, color="#FFB2D9", label="Normal")
-    fig.suptitle("Distribution of heart disease by " + feature)
-    ax.set_xlabel(feature)
-    ax.set_ylabel("Frequency")
-    ax.legend();
-    return data
+# check number of NaN values created
+print("===Check isnull()===")
+print(df.isnull().sum().sort_values(ascending=False))
 
-fig, ax = plt.subplots(figsize = (13, 6))
-histo_visualize(df, 'Sex')
+
+# 6. Data Visualization
 # 1) Visualization of Categorical features
 # Sex
-#fig, ax = plt.subplots(figsize = (13, 6))
-#ax.hist(df[df["HeartDisease"]==1]["Sex"], bins=2, alpha=0.5, color="#86E57F", label="HeartDisease")
-#ax.hist(df[df["HeartDisease"]==0]["Sex"], bins=2, alpha=0.5, color="#FFB2D9", label="Normal")
-#ax.set_xlabel("Sex")
-#ax.set_ylabel("Frequency")
-#fig.suptitle("Distribution of heart disease by Sex")
-#ax.legend();
+fig, ax = plt.subplots(figsize = (13, 6))
+ax.hist(df[df["HeartDisease"]==1]["Sex"], bins=2, alpha=0.5, color="#86E57F", label="HeartDisease")
+ax.hist(df[df["HeartDisease"]==0]["Sex"], bins=2, alpha=0.5, color="#FFB2D9", label="Normal")
+ax.set_xlabel("Sex")
+ax.set_ylabel("Frequency")
+fig.suptitle("Distribution of heart disease by Sex")
+ax.legend();
 
 # Smoking
 fig, ax = plt.subplots(figsize = (13,6))
@@ -95,65 +88,60 @@ sns.countplot( x= df['Race'], hue = 'HeartDisease', data = df, palette = 'pastel
 plt.xlabel('Race')
 plt.ylabel('Frequency')
 fig.suptitle("Distribution of heart disease by Race")
-#plt.show()
+
 # Age Category
 plt.figure(figsize = (13,6))
 sns.countplot(x = df['AgeCategory'], hue = 'HeartDisease', data = df, palette = 'pastel')
 fig.suptitle("Distribution of heart disease by AgeCategory")
 plt.xlabel('AgeCategory')
 plt.ylabel('Frequency')
-#plt.show()
+
 # Gen Health
 gen =  df['GenHealth'].unique()
 gen_cnt = df['GenHealth'].value_counts()
 sns.barplot(x=gen_cnt,y=gen,data=df, palette = 'pastel')
 fig.suptitle("Distribution of heart disease by AgeCategory")
-#plt.show()
 
 # KidneyDisease
 fig, ax = plt.subplots(figsize = (13,6))
 
 ax.hist(df[df["HeartDisease"]==1]["KidneyDisease"], bins=15, alpha=0.5, color="#86E57F", label="HeartDisease")
 ax.hist(df[df["HeartDisease"]==0]["KidneyDisease"], bins=15, alpha=0.5, color="#FFB2D9", label="Normal")
-
 ax.set_xlabel("KidneyDisease")
 ax.set_ylabel("Frequency")
 fig.suptitle("Distribution of heart disease by kidneydisease")
-
 ax.legend();
+
 # SkinCancer
 fig, ax = plt.subplots(figsize = (13,6))
 
 ax.hist(df[df["HeartDisease"]==1]["SkinCancer"], bins=15, alpha=0.5, color="#86E57F", label="HeartDisease")
 ax.hist(df[df["HeartDisease"]==0]["SkinCancer"], bins=15, alpha=0.5, color="#FFB2D9", label="Normal")
-
 ax.set_xlabel("SkinCancer")
 ax.set_ylabel("Frequency")
 fig.suptitle("Distribution of heart disease based on previous exposure to skin cancer")
-
 ax.legend();
+
 # Stroke
 fig, ax = plt.subplots(figsize = (13,6))
 
 ax.hist(df[df["HeartDisease"]==1]["Stroke"], bins=15, alpha=0.5, color="#86E57F", label="HeartDisease")
 ax.hist(df[df["HeartDisease"]==0]["Stroke"], bins=15, alpha=0.5, color="#FFB2D9", label="Normal")
-
 ax.set_xlabel("Stroke")
 ax.set_ylabel("Frequency")
 fig.suptitle("Distribution of heart disease based on previous exposure to Stroke")
-
 ax.legend();
+
 # Diabetic
 fig, ax = plt.subplots(figsize = (13,6))
 
 ax.hist(df[df["HeartDisease"]==1]["Diabetic"], bins=15, alpha=0.5, color="#86E57F", label="HeartDisease")
 ax.hist(df[df["HeartDisease"]==0]["Diabetic"], bins=15, alpha=0.5, color="#FFB2D9", label="Normal")
-
 ax.set_xlabel("Diabetic")
 ax.set_ylabel("Frequency")
 fig.suptitle("Distribution of heart disease based on previous exposure to Diabetic")
-
 ax.legend();
+
 plt.show()
 
 # 2) Visualization of Numerical Features
@@ -206,9 +194,11 @@ plt.title('Heatmap correlation of features')
 abs(correlation['HeartDisease']).sort_values()[:-1].plot.barh()
 plt.show()
 
+"""
+Data preprocessing
+"""
 
-# 7. Encoding for data(for categorical data)
-#enc = OneHotEncoder() 
+# 7. Encoding for data(for categorical data) 
 enc = LabelEncoder() 
 
 # Encoding categorical features - dataframe name : categ
@@ -216,17 +206,14 @@ encoded_categ = df[['Race']]
 encoded_categ = pd.DataFrame(enc.fit_transform(encoded_categ))
 # Linking the encoed_cateh with the df
 encoded_categ = pd.concat([df, encoded_categ], axis = 1)
-# Dropping the categorical features after encoding
+
 encoded_categ2 = df[['GenHealth']]
 encoded_categ2 = pd.DataFrame(enc.fit_transform(encoded_categ2))
 encoded_categ = pd.concat([encoded_categ, encoded_categ2], axis = 1)
+# Dropping the categorical features after encoding
 encoded_categ = encoded_categ.drop(columns = ['Race', 'GenHealth'], axis = 1)
 
-
-"""
-Data preprocessing
-"""
-# 6. Data Normalization
+# 8. Data Normalization
 '''
 StandardScaling change dataset by StandardScaler
 '''
@@ -265,18 +252,15 @@ mmTrainData=MinMaxScaling(['BMI','PhysicalHealth','MentalHealth','AgeCategory','
 rbTrainData=RobustScaling(['BMI','PhysicalHealth','MentalHealth','AgeCategory','SleepTime'],encoded_categ)
 
 data_list = [stTrainData, mmTrainData, rbTrainData]
-low_col = ['SleepTime', 'AlcoholDrinking', 'BMI', 'Sex', 'SkinCancer']
+# low_col : under correlation 1.5
+low_col = ['SleepTime', 'AlcoholDrinking', 'BMI', 'Sex', 'SkinCancer', 'PhysicalActivity', 'Smoking']
 size = 0.2
-
-
-# Use 3 normalized data
 
 from sklearn import tree
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
-
 
 """
 Show confusion matrix figure
@@ -290,21 +274,34 @@ def build_cm(test, predict):
     sns.heatmap(matrix, annot=True, fmt='d')
     plt.show()
 
-
+# Use 3 normalized data
 for i in range(3):
-    # 8. Prepare for split train dataset and test dataset
+    # 9. Prepare for split train dataset and test dataset
     data = data_list[i].drop(['HeartDisease'], axis=1) # drop target data
     data = data.drop(low_col, axis = 1) # drop data which has low correlation
     target = data_list[i]['HeartDisease']
 
     train_x, test_x, train_y, test_y = train_test_split(data, target, test_size=size, random_state=34, stratify = target, shuffle=True)
-    # 9. Build a model
-    # 1) Decision Tree model & KFold validation
+    # 10. Build a model
+    # 1) Random Forest Regressor model & GridSearchCV
+    f_param = {'n_estimators':(100, 200), 'max_depth':(5, 8), 'min_samples_leaf':(8, 18), 'min_samples_split':(5, 10)}
+    rf = RandomForestRegressor(random_state = 0, n_jobs=-1)
+    grid_rf = GridSearchCV(rf, param_grid=f_param, cv=KFold(5), n_jobs=-1)
+    grid_rf.fit(train_x, train_y)
+    # 11. Model evalution
+    # 1) Find best n_neighbors
+    print('best params: ', grid_rf.best_params_)
+    print('best score: ', grid_rf.best_score_)
+    print('best estimator: ', grid_rf.best_estimator_)
+
+    #######################################################
+    # 10. Build a model
+    # 2) Decision Tree model & KFold validation
     kfold = KFold(5, shuffle=True)
     dec = tree.DecisionTreeClassifier()
     score = cross_val_score(dec, X=train_x, y=train_y, cv=kfold) #train and predict
 
-    # 10. Model evalution
+    # 11. Model evalution
     # 1) Find validation score
     print("cross validation scroe: ", score)
     print("Mean score: ", np.mean(score))
@@ -314,79 +311,18 @@ for i in range(3):
     pred_y = dec.predict(test_x)
     build_cm(test_y, pred_y)
 
-    ########################################
-    # 9. Build a model
-    # 2) KNN classifier model & GridSearchCV
+    #######################################################
+    # 10. Build a model
+    # 3) KNN classifier model & GridSearchCV
     knn = KNeighborsClassifier()
     h_param = {'n_neighbors':np.arange(1, 10, 2)} #list [1, 3, 5, 7, 9]
     
     grid_knn = GridSearchCV(knn, param_grid=h_param, cv=KFold(5), n_jobs=-1)
     grid_knn.fit(train_x, train_y)
-    # 10. Model evalution
+    # 11. Model evalution
     # 1) Find best n_neighbors
     print('best params: ', grid_knn.best_params_)
     print('best score: ', grid_knn.best_score_)
     print('best estimator: ', grid_knn.best_estimator_)
     # 2) Confusion matrix
     build_cm(test_y, grid_knn.predict(test_x))
-
-
-
-    
-
-# 8. Prepare for split train dataset and test dataset
-#data = stTrainData.drop(['HeartDisease'], axis=1) # drop target data
-#data = data.drop(['SleepTime', 'AlcoholDrinking', 'BMI', 'Sex', 'SkinCancer'], axis = 1) # drop data which has low correlation
-#target = stTrainData['HeartDisease']
-
-# 9. Build a model
-########################################
-# 1) Decision Tree model & KFold validation
-#size = 0.2
-#train_x, test_x, train_y, test_y = train_test_split(data, target, test_size=size, random_state=34, stratify = target, shuffle=True)
-#kfold = KFold(5, shuffle=True)
-#model = tree.DecisionTreeClassifier()
-#score = cross_val_score(model, X=train_x, y=train_y, cv=kfold) #train and predict
-# 10. Model evalution
-# 1) Find validation score
-#print("cross validation scroe: ", score)
-#print("Mean score: ", np.mean(score))
-
-#model.fit(train_x, train_y)
-#pred_y = model.predict(test_x)
-
-# 2) Confusion matrix
-#cm = confusion_matrix(test_y, model.predict(test_x))
-#matrix = pd.DataFrame(data = cm, columns = ['Predicted:0', 'Predicted:1'], index = ['Actual:0', 'Actual:1'])
-#plt.figure(figsize = (8, 5))
-#sns.heatmap(matrix, annot=True, fmt='d')
-#plt.show()
-
-
-########################################
-# 2) KNN classifier model & GridSearchCV
-'''
-knn = KNeighborsClassifier()
-train_x, test_x, train_y, test_y = train_test_split(data, target, test_size=size, random_state=34, stratify = target, shuffle=True)
-h_param = {'n_neighbors':np.arange(1, 10, 2)} #list [1, 3, 5, 7, 9]
-
-grid_knn = GridSearchCV(knn, param_grid=h_param, cv=KFold(5), score='accuracy', n_jobs=-1)
-grid_knn.fit(train_x, train_y)
-pred_y = grid_knn.predict(test_x)
-
-# 10. Model evalution
-# 1) Find best n_neighbors
-print('best params: ', grid_knn.best_params_)
-print('best score: ', grid_knn.best_score_)
-print('best estimator: ', grid_knn.best_estimator_)
-
-
-# 2) Confusion matrix
-cm = confusion_matrix(test_y, pred_y)
-matrix = pd.DataFrame(data = cm, columns = ['Predicted:0', 'Predicted:1'], index = ['Actual:0', 'Actual:1'])
-plt.figure(figsize = (8, 5))
-sns.heatmap(matrix, annot=True, fmt='d')
-plt.show()
-
-
-'''
